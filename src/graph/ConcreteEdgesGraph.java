976bfe9dc7db1f3c750406cc19a4be2b3753eba3
geetu040 <raoarmaghanshakir040@gmail.com>
2023-11-19 20:33:24 +0500

    Putting the Implementation of Edges and Vertices

--- src/graph/ConcreteEdgesGraph.java
+++ src/graph/ConcreteEdgesGraph.java
@@ -1,88 +1,182 @@
-/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package graph;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * An implementation of Graph.
  * 
  * <p>PS2 instructions: you MUST use the provided rep.
  */
-public class ConcreteEdgesGraph implements Graph<String> {
+public class ConcreteEdgesGraph extends Graph<String> {
     
-    private final Set<String> vertices = new HashSet<>();
+	public ConcreteEdgesGraph(Set<String> vertices) {
+	    super(vertices);
+
+	    // Initialize edges field
+	    for (String vertex : vertices) {
+	        edges.add(new Edge(vertex, vertex, 0)); // Assume a self-loop with weight 0 for each vertex
+	    }
+	}
+
+
+	private final Set<String> vertices = new HashSet<>();
     private final List<Edge> edges = new ArrayList<>();
     
     // Abstraction function:
-    //   TODO
+    //   Represents a weighted directed graph with string-labeled vertices and edges.
+    //   The set 'vertices' contains all vertices in the graph.
+    //   The list 'edges' contains all directed edges with their corresponding weights.
+    
     // Representation invariant:
-    //   TODO
+    //   - Every edge in 'edges' must have valid source and target vertices that exist in 'vertices'.
+    //   - There should be no duplicate edges with the same source and target vertices.
+    
     // Safety from rep exposure:
-    //   TODO
+    //   - All fields are private and final.
+    //   - 'vertices' is returned as an unmodifiable set to prevent modification.
+    //   - 'edges' is not exposed outside of the class.
     
-    // TODO constructor
+//    // Constructor
+//    public ConcreteEdgesGraph() {
+//        checkRep();
+//    }
     
-    // TODO checkRep
-    
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+    // Check the representation invariant
+    private void checkRep() {
+        for (Edge edge : edges) {
+            assert vertices.contains(edge.getSource()) : "Invalid edge source vertex";
+            assert vertices.contains(edge.getTarget()) : "Invalid edge target vertex";
+        }
+        Set<Edge> uniqueEdges = new HashSet<>(edges);
+        assert uniqueEdges.size() == edges.size() : "Duplicate edges found";
     }
     
-    @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+    public boolean add(String vertex) {
+        boolean added = vertices.add(vertex);
+        checkRep();
+        return added;
     }
     
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    // TODO toString()
-    
-}
 
-/**
- * TODO specification
- * Immutable.
- * This class is internal to the rep of ConcreteEdgesGraph.
- * 
- * <p>PS2 instructions: the specification and implementation of this class is
- * up to you.
- */
-class Edge {
+    public int set(String source, String target, int weight) {
+        Edge newEdge = new Edge(source, target, weight);
+        Edge existingEdge = edges.stream()
+                .filter(edge -> edge.getSource().equals(source) && edge.getTarget().equals(target))
+                .findFirst()
+                .orElse(null);
+        
+        if (existingEdge != null) {
+            int previousWeight = existingEdge.getWeight();
+            existingEdge.setWeight(weight);
+            checkRep();
+            return previousWeight;
+        } else {
+            edges.add(newEdge);
+            checkRep();
+            return 0;
+        }
+    }
     
-    // TODO fields
+    public boolean remove(String vertex) {
+        boolean removed = vertices.remove(vertex);
+        edges.removeIf(edge -> edge.getSource().equals(vertex) || edge.getTarget().equals(vertex));
+        checkRep();
+        return removed;
+    }
     
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
+    public Set<String> vertices() {
+        return Collections.unmodifiableSet(vertices);
+    }
     
-    // TODO constructor
+    public Map<String, Integer> sources(String target) {
+        Map<String, Integer> sourceVertices = new HashMap<>();
+        for (Edge edge : edges) {
+            if (edge.getTarget().equals(target)) {
+                sourceVertices.put(edge.getSource(), edge.getWeight());
+            }
+        }
+        return sourceVertices;
+    }
     
-    // TODO checkRep
+    public Map<String, Integer> targets(String source) {
+        Map<String, Integer> targetVertices = new HashMap<>();
+        for (Edge edge : edges) {
+            if (edge.getSource().equals(source)) {
+                targetVertices.put(edge.getTarget(), edge.getWeight());
+            }
+        }
+        return targetVertices;
+    }
     
-    // TODO methods
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Edge edge : edges) {
+            sb.append(edge.toString()).append("\n");
+        }
+        return sb.toString();
+    }
     
-    // TODO toString()
-    
+    /**
+     * An edge in the graph.
+     * 
+     * <p>PS2 instructions: the specification and implementation of this class are
+     * up to you.
+     */
+    static class Edge {
+        
+        private final String source;
+        private final String target;
+        private int weight;
+        
+        // Abstraction function:
+        //   Represents a directed edge with a source vertex, target vertex, and weight.
+        
+        // Representation invariant:
+        //   - 'source' and 'target' must not be null.
+        //   - 'weight' must be non-negative.
+        
+        // Safety from rep exposure:
+        //   - All fields are private and final, except 'weight', which is mutable.
+        
+        // Constructor
+        public Edge(String source, String target, int weight) {
+            this.source = source;
+            this.target = target;
+            this.weight = weight;
+            checkRep();
+        }
+        
+        // Check the representation invariant
+        private void checkRep() {
+            assert source != null : "Source vertex cannot be null";
+            assert target != null : "Target vertex cannot be null";
+            assert weight >= 0 : "Weight must be non-negative";
+        }
+        
+        // Getters
+        public String getSource() {
+            return source;
+        }
+        
+        public String getTarget() {
+            return target;
+        }
+        
+        public int getWeight() {
+            return weight;
+        }
+        
+        // Setters
+        public void setWeight(int weight) {
+            this.weight = weight;
+            checkRep();
+        }
+        
+        // TODO: Implement any additional methods
+        
+        @Override
+        public String toString() {
+            return String.format("%s -> %s : %d", source, target, weight);
+        }
+    }
 }
--- src/graph/ConcreteVerticesGraph.java
+++ src/graph/ConcreteVerticesGraph.java
@@ -1,9 +1,8 @@
-/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package graph;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,74 +12,159 @@ import java.util.Set;
  * 
  * <p>PS2 instructions: you MUST use the provided rep.
  */
-public class ConcreteVerticesGraph implements Graph<String> {
-    
+public class ConcreteVerticesGraph extends Graph<String> {
+
     private final List<Vertex> vertices = new ArrayList<>();
-    
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
-    
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+
+    /**
+     * Abstraction function:
+     *   Represents a directed graph with labeled vertices. Each vertex is
+     *   associated with a Vertex object in the 'vertices' list.
+     * 
+     * Representation invariant:
+     *   - Each vertex label is unique.
+     *   - The 'vertices' list contains all the vertices in the graph.
+     * 
+     * Safety from rep exposure:
+     *   - The 'vertices' list is private and final. Any external modifications
+     *     to the list are prevented.
+     *   - Methods that return information about vertices (e.g., 'vertices()',
+     *     'sources()', 'targets()') return copies of data to avoid exposing the
+     *     internal representation.
+     */
+
+    // Constructor
+    public ConcreteVerticesGraph(Set<String> vertices) {
+        super(vertices);
+        for (String vertexLabel : vertices) {
+            this.vertices.add(new Vertex(vertexLabel));
+        }
     }
-    
-    @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+
+    // Representation invariant
+    private void checkRep() {
+        Set<String> labels = new HashSet<>();
+        for (Vertex vertex : vertices) {
+            String label = vertex.getLabel();
+            if (labels.contains(label)) {
+                throw new RuntimeException("Duplicate vertex label: " + label);
+            }
+            labels.add(label);
+        }
     }
-    
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public boolean add(String vertex) {
+        if (super.add(vertex)) {
+            vertices.add(new Vertex(vertex));
+            checkRep();
+            return true;
+        }
+        return false;
     }
-    
-    @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public int set(String source, String target, int weight) {
+        // Implementation of set method...
+        checkRep();
+        return 0; // Placeholder, replace with actual implementation
     }
-    
-    @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public boolean remove(String vertex) {
+        if (super.remove(vertex)) {
+            vertices.removeIf(v -> v.getLabel().equals(vertex));
+            checkRep();
+            return true;
+        }
+        return false;
     }
-    
-    @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public Set<String> vertices() {
+        Set<String> vertexSet = new HashSet<>();
+        for (Vertex vertex : vertices) {
+            vertexSet.add(vertex.getLabel());
+        }
+        return vertexSet;
     }
-    
-    // TODO toString()
-    
+
+    @Override
+    public Map<String, Integer> sources(String target) {
+        // Implementation of sources method...
+        return Collections.emptyMap(); // Placeholder, replace with actual implementation
+    }
+
+    @Override
+    public Map<String, Integer> targets(String source) {
+        // Implementation of targets method...
+        return Collections.emptyMap(); // Placeholder, replace with actual implementation
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Vertices: [");
+        for (Vertex vertex : vertices) {
+            result.append(vertex).append(", ");
+        }
+        if (!vertices.isEmpty()) {
+            result.delete(result.length() - 2, result.length()); // Remove the trailing comma and space
+        }
+        result.append("]");
+        return result.toString();
+    }
+
 }
 
 /**
- * TODO specification
- * Mutable.
- * This class is internal to the rep of ConcreteVerticesGraph.
- * 
- * <p>PS2 instructions: the specification and implementation of this class is
- * up to you.
+ * Represents a vertex in a directed graph.
  */
 class Vertex {
-    
-    // TODO fields
-    
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
-    
-    // TODO constructor
-    
-    // TODO checkRep
-    
+
+    private final String label;
+
+    /**
+     * Abstraction function:
+     *   Represents a vertex in a directed graph with a unique label.
+     * 
+     * Representation invariant:
+     *   - 'label' is not null.
+     * 
+     * Safety from rep exposure:
+     *   - 'label' is private and final.
+     */
+
+    // Constructor
+    public Vertex(String label) {
+        this.label = label;
+        checkRep();
+    }
+
+    // Representation invariant
+    private void checkRep() {
+        if (label == null) {
+            throw new RuntimeException("Vertex label cannot be null.");
+        }
+    }
+
+    /**
+     * Get the label of the vertex.
+     * 
+     * @return The label of the vertex.
+     */
+    public String getLabel() {
+        return label;
+    }
+
     // TODO methods
-    
-    // TODO toString()
-    
+
+    @Override
+    public String toString() {
+        return label;
+    }
+
 }
+
+
+
+
